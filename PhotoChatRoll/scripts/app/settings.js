51dbe2381e496c0f8f21407f2784d8638f1a0c5b
Ivan Stefanov <inj.stefanov@gmail.com>
2014-10-17 18:07:47 +0000

    Changes made by Ivan Stefanov (inj.stefanov@gmail.com).

--- PhotoChatRoll/scripts/app/settings.js
+++ PhotoChatRoll/scripts/app/settings.js
@@ -5,7 +5,7 @@
 var appSettings = {
 
     everlive: {
-        apiKey: '$EVERLIVE_API_KEY$', // Put your Backend Services API key here
+        apiKey: 'WuNYNTNdWfNS47B5', // Put your Backend Services API key here
         scheme: 'http'
     },
 

8705b3988ec956618469ea5bcdc0ba289a988e5c
Ivan Stefanov <inj.stefanov@gmail.com>
2014-10-18 06:59:23 +0000

    Changes made by Ivan Stefanov (inj.stefanov@gmail.com).

--- PhotoChatRoll/scripts/app/rollette.js
+++ PhotoChatRoll/scripts/app/rollette.js
@@ -5,20 +5,32 @@ var app = app || {};
 
 app.Rollette = (function () {
     'use strict';
-    
-    var roletteViewModel = (function()
-    {
-        var show = function(){
-            var currentUser = app.Users.currentUser;
-            alert(JSON.stringify(currentUser));
-            if(currentUser.data !== null)
-                $('#InitiatorProfile img').attr('src', currentUser.data.Picture);
-        };
-        
-        return {
-            show: show
-        };
-    }());
-    
-    return roletteViewModel;
+
+    var roletteViewModel = (function () {
+            var show = function () {
+                var currentUser = app.Users.currentUser;
+                alert(JSON.stringify(currentUser));
+                if (currentUser.data !== null)
+                    $('#InitiatorProfile img').attr('src', currentUser.data.Picture);
+            };
+
+            var init = function () {
+                $('#counter_2').countdown({
+                    image: '../../styles/images/digits.png',
+                    startTime: '00:05',
+                    timerEnd: function () {
+                        alert('end!');
+                    },
+                    format: 'mm:ss'
+                });
+            });
+    }
+
+    return {
+        show: show,
+        init: init
+    };
+}());
+
+return roletteViewModel;
 }());
